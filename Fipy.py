b24a82da668fa9c65c153717171325564a90e04b
San096 <sanderleycc2022@alu.ufc.br>
2025-03-17 23:13:38 -0300

    Primeira versão_tabela_fipe

--- Fipy.py
+++ Fipy.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+import json
 
 # Configurações do navegador
 options = Options()
@@ -15,37 +16,56 @@ navegador = webdriver.Chrome(options=options)
 navegador.get("https://veiculos.fipe.org.br/")
 navegador.maximize_window()
 
-# Aguarda e clica no elemento
+# Aguarda e clica no primeiro elemento (Consulta de carros)
 WebDriverWait(navegador, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="front"]/div[1]/div[2]/ul/li[1]/a/div[1]'))).click()
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="front"]/div[1]/div[2]/ul/li[1]/a/div[1]'))
+).click()
 
 time.sleep(1)
-#segundo :clicar e selecionar o mes de pesquisa
+
+# **Selecionar mês de pesquisa**
 navegador.find_element(By.XPATH, '//*[@id="selectTabelaReferenciacarro_chosen"]/a/div/b').click()
-options_mes_ano = navegador.find_elements(By.XPATH, '//*[@id="selectTabelaReferenciacarro_chosen"]/div/ul')
-lista_mes_ano = options_mes_ano[0].find_elements(By.CSS_SELECTOR,'li')
-lista_mes_ano[1].click()
+options_mes_ano = navegador.find_element(By.XPATH, '//*[@id="selectTabelaReferenciacarro_chosen"]/div/ul')
+lista_mes_ano = options_mes_ano.find_elements(By.CSS_SELECTOR, 'li')
+lista_mes_ano[1].click()  # Escolhe o segundo item
 
-#Terceiro :clicar e selecionar marca 
+# **Selecionar marca**
 navegador.find_element(By.XPATH, '//*[@id="selectMarcacarro_chosen"]/a/div/b').click()
-options_marca = navegador.find_elements(By.XPATH, '//*[@id="selectMarcacarro_chosen"]/div/ul')
-lista_marca = options_marca[0].find_elements(By.CSS_SELECTOR,'li')
-lista_marca[2].click()
+options_marca = navegador.find_element(By.XPATH, '//*[@id="selectMarcacarro_chosen"]/div/ul')
+lista_marca = options_marca.find_elements(By.CSS_SELECTOR, 'li')
+lista_marca[0].click()  # Escolhe a primeira marca disponível
 
-#Quarto clicar e selecionar modelo 
+# **Selecionar modelo**
 navegador.find_element(By.XPATH, '//*[@id="selectAnoModelocarro_chosen"]/a/div/b').click()
-options_modelo = navegador.find_elements(By.XPATH, '//*[@id="selectAnoModelocarro_chosen"]/div/ul')
-lista_modelo = options_modelo[0].find_elements(By.CSS_SELECTOR,'li')
-lista_modelo[3].click()
+options_modelo = navegador.find_element(By.XPATH, '//*[@id="selectAnoModelocarro_chosen"]/div/ul')
+lista_modelo = options_modelo.find_elements(By.CSS_SELECTOR, 'li')
+lista_modelo[1].click()  # Escolhe o segundo modelo disponível
 
-#Quinto : clicar e selecuonar ano/modelo 
+# **Selecionar ano/modelo**
 navegador.find_element(By.XPATH, '//*[@id="selectAnocarro_chosen"]/a/div/b').click()
-options_ano = navegador.find_elements(By.XPATH, '//*[@id="selectAnocarro_chosen"]/div/ul')
-lista_ano = options_ano[0].find_elements(By.CSS_SELECTOR,'li')
-lista_ano[0].click()
-#sexto : clicar em pesquisar 
-navegador.find_element(By.LINK_TEXT,'Pesquisar').click()
+options_ano = navegador.find_element(By.XPATH, '//*[@id="selectAnocarro_chosen"]/div/ul')
+lista_ano = options_ano.find_elements(By.CSS_SELECTOR, 'li')
+lista_ano[0].click()  # Escolhe o primeiro ano disponível
 
-#Setimo: salvar os dados da tabela em um dicionarios;
+# **Clicar em "Pesquisar"**
+navegador.find_element(By.XPATH, '//*[@id="buttonPesquisarcarro"]').click()
 
+# **Aguardar resultado da pesquisa**
+WebDriverWait(navegador, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="resultadoConsultacarroFiltros"]/table/tbody'))
+)
 
+# **Extrair os dados da tabela e salvar em um dicionário**
+tabela = navegador.find_element(By.XPATH, '//*[@id="resultadoConsultacarroFiltros"]/table/tbody')
+linhas = tabela.find_elements(By.CSS_SELECTOR, 'td')
+
+carro = {}
+
+for item in range(0, len(linhas) - 1, 2):
+    carro[linhas[item].text] = linhas[item + 1].text
+
+    navegador.close()
+
+object_json = json.dumps(carro, indent = 2, ensure_ascii = False) #Função para cria um arqui json som as informações 
+with open('carro.json', 'w') as file:
+    file.write(object_json)
